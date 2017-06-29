94f96ace4e936d68c758cbc350eab00c6f7fc365
babagaba11262 <lauri.partanen1991@gmail.com>
2017-06-29 20:30:19 +0300

    1

---
+++ koodi.py
@@ -0,0 +1,28 @@
+# Tehdään numeroiden lajittelu ohjelma.
+# 1. käy läpi numerot alkaen 0 tai siitä mihin on jääty, joka katsotaan teksti tiedostosta.
+# 2. jokaisen numeron kohdalla tehdään tarkistuksia ->
+#    -jaollisuus (numeroon itseensä asti, eli onko kokonaisluku)
+#    -onko neliöjuuri tasaluku
+#    -logaritmi (tämä vaatii hieman miettimistä mitä kaikkea tästä halutaan)
+#       log* hakee luvusta sen exponentin mikä tulee kun * korotetaan tähän exponenttiin.
+#       esim log2 8 = 3 koska 2*2*2 = 8
+     
+
+#    Läpi käytyjen lukujen tallennus:
+#    -tallennetaan tekstitiedostoon
+#    -1 luku joka riville, luvun perään : ja sen jälkeen luvun tagit ja muut tiedot, eroteltuna ; esim.
+#    KL = kokonaisluku, LOG:X, TSQR = Tasainen neliöjuuri,
+
+
+#    Kerättyjen tietojen näyttäminen:
+#    graafinen ikkuna, josta voi hakea jotain numeroa, tai katsella isoa listaa, jota voi suodattaa. suodattimet voi
+#    kääntää ympäri, eli voi päättää näytetäänkö luvut jotka ovat alkulukuja vai ne jotka eivät ole
+
+#    Muuta
+#    tekstitiedoston kokoa pitää seurata, ja laittaa joku raja sen koolle.
+
+
+
+
+#ensin tehdään funktio, joka pyörittää lukuja
+def
