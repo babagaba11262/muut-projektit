5bd0fe12d1f1f9cb98048df9730ebc381abe82d4
babagaba11262 <lauri.partanen1991@gmail.com>
2017-06-29 20:50:42 +0300

    1

--- numerolajittelu_koodi.py
+++ numerolajittelu_koodi.py
@@ -1,5 +1,5 @@
 # Tehdään numeroiden lajittelu ohjelma.
-# 1. käy läpi numerot alkaen 0 tai siitä mihin on jääty, joka katsotaan teksti tiedostosta.
+# 1. käy läpi numerot alkaen 0 tai siitä mihin on jääty, joka katsotaan numerolajittelu_teksti tiedostosta.
 # 2. jokaisen numeron kohdalla tehdään tarkistuksia ->
 #    -jaollisuus (numeroon itseensä asti, eli onko kokonaisluku)
 #    -onko neliöjuuri tasaluku
@@ -25,4 +25,29 @@
 
 
 #ensin tehdään funktio, joka pyörittää lukuja
-def
+def numeropyoritys(raja):
+    numero = 0
+    while numero <= raja:
+        numero += 1
+
+    return numero
+
+#tehdään userimput funktio, joka sisältää myös perus virheentarkistus systeemit
+def userinput():
+    a
+
+#tehdään funktio joka katsoo onko luku kokonaisluku
+
+
+def kokonaisluku(luku):
+
+    a = True
+    tags = ""
+    while a is True:
+        for a2 in range(0, luku, 1):
+            if luku % a2 == 0:
+                tags = "KL"
+
+        a = False
+    return tags
+
